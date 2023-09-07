bee7465681a88ac4df764c6b441fa7696092d8b0
Soakeson <skyler.oakeson@gmail.com>
2023-09-07 08:49:17 -0600

    Fixed treesitter to work with svelte.

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -2,6 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
     require('nvim-treesitter.configs').setup({
+      ensure_installed = {'lau', 'vim', 'typescript', 'svelte'},
+      sync_install = false,
+      auto_install = true,
       highlight = {
         enable = true
       }
