050c8bde219c71a6dafadd4eeba94424a60a7377
Soakeson <skyler.oakeson@gmail.com>
2023-09-27 12:54:10 -0600

    Cleaned up colorschemes and added two.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,20 +1,16 @@
 return {
-  { 'mcchrish/zenbones.nvim', dependencies = { 'rktjmp/lush.nvim' }, },
-  { 'ntk148v/komau.vim', priority = 1000,},
-  { 'fxn/vim-monochrome', },
   { 'kxzk/skull-vim', },
   { 'pineapplegiant/spaceduck' },
   { 'rebelot/kanagawa.nvim' },
-  { 'olivercederborg/poimandres.nvim', },
   { 'sainnhe/everforest' },
-  { 'KabbAmine/yowish.vim' },
-  { 'srcery-colors/srcery-vim' },
   { 'sainnhe/gruvbox-material' },
   { 'aktersnurra/no-clown-fiesta.nvim' },
   { 'Everblush/everblush.vim' },
   { 'joshdick/onedark.vim' },
+  { 'shaunsingh/moonlight.nvim' },
+  { 'lourenci/github-colors' },
   { 'ayu-theme/ayu-vim',
     config=function()
-      vim.cmd[[colorscheme onedark]]
+      vim.cmd[[colorscheme ayu]]
     end }
   }
