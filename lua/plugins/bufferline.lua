408f23fbc740f6af8525c45c1950caf90f8df1a4
Soakeson <skyler.oakeson@gmail.com>
2023-09-30 12:52:56 -0600

    Nvim tree now float in middle.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,11 +5,19 @@ return {
   config = function()
   require("bufferline").setup{
     options = {
+        themable = true,
+        offsets = {
+          filetype = 'NvimTree',
+          text = 'File Explorer',
+          text_align = 'center',
+          seprator = true,
+        },
         hover = {
             enabled = true,
             delay = 150,
             reveal = {'close'}
-        }
+        },
+      sparator_style = 'slant'
     }
   }
   end
