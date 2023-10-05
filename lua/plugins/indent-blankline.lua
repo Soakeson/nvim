764ab513c1c8291a221a529255ad4ffd0b89e59c
Soakeson <skyler.oakeson@gmail.com>
2023-10-05 13:42:01 -0600

    Fixed blankline.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,1 +1,8 @@
-return { 'lukas-reineke/indent-blankline.nvim', }
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts ={},
+  config = function()
+    require('ibl').setup()
+  end
+}
