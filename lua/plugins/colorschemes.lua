75ab4a81a57d3d54ebbd32094356a75e960a8404
Soakeson <skyler.oakeson@gmail.com>
2023-10-12 07:47:13 -0600

    Added a good github color; Removed ones not being used.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,23 +1,17 @@
 return {
   { 'kxzk/skull-vim', },
-  { 'pineapplegiant/spaceduck' },
   { 'rebelot/kanagawa.nvim' },
   { 'sainnhe/everforest' },
   { 'sainnhe/gruvbox-material' },
   { 'aktersnurra/no-clown-fiesta.nvim' },
-  { 'Everblush/everblush.vim' },
   { 'joshdick/onedark.vim' },
   { 'shaunsingh/moonlight.nvim' },
-  { 'lourenci/github-colors' },
   { 'bluz71/vim-moonfly-colors'},
   { 'w0ng/vim-hybrid' },
-  { 'lunacookies/vim-corvine' },
   { 'xero/miasma.nvim' },
   { 'tomasiser/vim-code-dark'},
-  { 'kvrohit/rasmus.nvim',
-    config = function()
-    end
-  },
+  { 'projekt0n/github-nvim-theme' },
+  { 'kvrohit/rasmus.nvim' },
   { 'ayu-theme/ayu-vim',
     config=function()
       vim.cmd[[colorscheme no-clown-fiesta]]
